c66237dc6989d9b5325600f3e9147d5754220981
nitesh <nitneverlost@gmail.com>
2012-12-05 19:06:19 +0530

    first commit

---
+++ js/variable.js
@@ -0,0 +1,25 @@
+//primitive = number , boolean -- > stack 
+//"string"
+// number -- > 12;
+// string -- > "12";
+// boolean --> true;
+//-->undefined
+
+//heap -- > string 
+//--array;
+//--object
+
+// var arr = [10, 20, 30 , 40] ;
+// var obj = {
+// 	"firstName": "teja",
+// 	"lastName": "nagee", 
+// 	"occupataion": "web dev"
+// }
+
+//variable naming rules - start with alphabet , $ , _
+
+// var first_name = "";
+// var $first_name = "";
+// var _firstName = "";
+
+
